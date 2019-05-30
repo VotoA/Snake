16c7951e0dd51c2c06c3f41d740155fe34cc60ee
VotoA <31769795+VotoA@users.noreply.github.com>
2019-05-29 23:30:23 -0500

    Add files via upload

--- Snake/SnakeEvolution.py
+++ Snake/SnakeEvolution.py
@@ -6,11 +6,14 @@ import pyautogui as gui
 import operator
 import copy
 
-def run_game(counttime, nNetwork, gen):
+def run_game(counttime, nNetwork, show):
 	failed = False
 	pg.init()
-	screen = pg.display.set_mode((755, 755))
-	pg.display.set_caption("Snake")
+	if show == True:
+		screen = pg.display.set_mode((755, 755))
+		pg.display.set_caption("Snake")
+	else:
+		screen = 1
 	snake = Snake()
 	food = Food()
 	score = 0
@@ -21,13 +24,14 @@ def run_game(counttime, nNetwork, gen):
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
 				sys.exit()
-		screen.fill((0,0,0))
+		if show == True:
+			screen.fill((0,0,0))
 		if count % counttime == 0:
-			if 15 - count/counttime > 0:
-				score += 15 - count/counttime
+			if 101 - count/counttime > 0:
+				score += 1
 			foodDirection = snake.getRelativeFoodPosition(food)
 			adjObstacles = snake.getAdjObstacles()
-			decision = nNetwork.evaluate([foodDirection[0], foodDirection[1], foodDirection[2], adjObstacles[0], adjObstacles[1], adjObstacles[2], adjObstacles[3], adjObstacles[4], adjObstacles[5], adjObstacles[6], 1])
+			decision = nNetwork.evaluate([foodDirection[0], foodDirection[1], foodDirection[2], adjObstacles[0], adjObstacles[1], adjObstacles[2], adjObstacles[3], adjObstacles[4], adjObstacles[5], adjObstacles[6], adjObstacles[7], adjObstacles[8], adjObstacles[9], 1])
 			if decision[0] >= decision[1] and decision[0] >= decision[2]:
 				finalMove = snake.direction+1
 			elif decision[1] >= decision[0] and decision[1] >= decision[2]:
@@ -39,15 +43,17 @@ def run_game(counttime, nNetwork, gen):
 			elif finalMove == 5:
 				finalMove = 1
 			snake.changeDirection(finalMove)
-			food.draw(screen)
+			if show == True:
+				food.draw(screen)
 			if snake.position == food.position:
 				food.generate(snake.positions)
-				snake.updateLength(screen)
+				snake.updateLength(screen, show)
 				score += 150
 				timer = 0
 			else:
-				snake.update(screen, False)
-			pg.display.flip()
+				snake.update(screen, False, show)
+			if show == True:
+				pg.display.flip()
 			if snake.position[0] < 0 or snake.position[0] > 751 or snake.position[1] < 0 or snake.position[1] > 751:
 				failed = True
 			c = 0
@@ -72,12 +78,12 @@ class Snake():
 	def changeDirection(self, direction):
 		if not (self.direction - direction) % 2 == 0:
 			self.direction = direction
-	def updateLength(self, screen):
+	def updateLength(self, screen, show):
 		self.length+=1
 		if self.length == 900:
 			boolean = True
-		self.update(screen, True)
-	def update(self, screen, new):
+		self.update(screen, True, show)
+	def update(self, screen, new, show):
 		if new == False and self.positions:
 			self.positions.pop(0)
 		if self.direction == 1:
@@ -88,9 +94,10 @@ class Snake():
 			self.position = ((self.position[0]-25), (self.position[1]))
 		elif self.direction == 4:
 			self.position = ((self.position[0]), (self.position[1]+25))
-		pg.draw.rect(screen, (255,255,255), (self.position[0], self.position[1], 20, 20))
-		for pos in self.positions:
-			pg.draw.rect(screen, (255,255,255), (pos[0], pos[1], 20, 20))
+		if show == True:
+			pg.draw.rect(screen, (255,255,255), (self.position[0], self.position[1], 20, 20))
+			for pos in self.positions:
+				pg.draw.rect(screen, (255,255,255), (pos[0], pos[1], 20, 20))
 		self.positions.append(self.position)
 	def getRelativeFoodPosition(self, food):
 		rel = map(lambda x, y: y - x, self.position, food.position)
@@ -126,25 +133,33 @@ class Snake():
 				return [1, 0, 0]
 		return [0, 1, 0]
 	def getAdjObstacles(self):
-		absolute = [0, 0, 0, 0, 0, 0, 0, 0]
-		relative = [0, 0, 0, 0, 0, 0, 0,]
+		absolute = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		relative = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 		for pos in self.positions:
-			if self.position[0]+25 == pos[0] and self.position[1] == pos[1] or self.position[0]+25 > 751:
+			if self.position[0]+25 == pos[0] and self.position[1] == pos[1]:
 				absolute[0] = 1
-			if self.position[0]+25 == pos[0] and self.position[1]-25 == pos[1] or self.position[1]-25 < 0:
+			if self.position[0]+25 == pos[0] and self.position[1]-25 == pos[1]:
 				absolute[1] = 1
-			if self.position[0] == pos[0] and self.position[1]-25 == pos[1] or self.position[0]-25 < 0:
+			if self.position[0] == pos[0] and self.position[1]-25 == pos[1]:
 				absolute[2] = 1
-			if self.position[0]-25 == pos[0] and self.position[1]-25 == pos[1] or self.position[1]+25 > 751:
+			if self.position[0]-25 == pos[0] and self.position[1]-25 == pos[1]:
 				absolute[3] = 1
-			if self.position[0]-25 == pos[0] and self.position[1] == pos[1] or self.position[0]+25 > 751:
+			if self.position[0]-25 == pos[0] and self.position[1] == pos[1]:
 				absolute[4] = 1
-			if self.position[0]-25 == pos[0] and self.position[1]+25 == pos[1] or self.position[1]-25 < 0:
+			if self.position[0]-25 == pos[0] and self.position[1]+25 == pos[1]:
 				absolute[5] = 1
-			if self.position[0] == pos[0] and self.position[1]+25 == pos[1] or self.position[0]-25 < 0:
+			if self.position[0] == pos[0] and self.position[1]+25 == pos[1]:
 				absolute[6] = 1
-			if self.position[0]+25 == pos[0] and self.position[1]+25 == pos[1] or self.position[1]+25 > 751:
+			if self.position[0]+25 == pos[0] and self.position[1]+25 == pos[1]:
 				absolute[7] = 1
+			if self.position[0]+50 == pos[0] and self.position[1] == pos[1]:
+				absolute[8] = 1
+			if self.position[0] == pos[0] and self.position[1]-50 == pos[1]:
+				absolute[9] = 1
+			if self.position[0]-50 == pos[0] and self.position[1] == pos[1]:
+				absolute[10] = 1
+			if self.position[0] == pos[0] and self.position[1]+50 == pos[1]:
+				absolute[11] = 1
 		if self.position[0]+25 > 751:
 			absolute[0] = 1
 		if self.position[1]-25 < 0:
@@ -161,6 +176,14 @@ class Snake():
 			absolute[5] = 1
 		if self.position[0]+25 > 751 and self.position[1]+25 > 751:
 			absolute[7] = 1
+		if self.position[0]+50 > 751:
+			absolute[8] = 1
+		if self.position[1]-50 < 0:
+			absolute[9] = 1
+		if self.position[0]-50 < 0:
+			absolute[10] = 1
+		if self.position[1]+50 > 751:
+			absolute[11] = 1
 		if self.direction == 1:
 			relative[0] = absolute[3]
 			relative[1] = absolute[2]
@@ -169,6 +192,9 @@ class Snake():
 			relative[4] = absolute[7]
 			relative[5] = absolute[6]
 			relative[6] = absolute[5]
+			relative[7] = absolute[9]
+			relative[8] = absolute[8]
+			relative[9] = absolute[11]
 		elif self.direction == 2:
 			relative[0] = absolute[5]
 			relative[1] = absolute[4]
@@ -177,6 +203,9 @@ class Snake():
 			relative[4] = absolute[1]
 			relative[5] = absolute[0]
 			relative[6] = absolute[7]
+			relative[7] = absolute[10]
+			relative[8] = absolute[9]
+			relative[9] = absolute[8]
 		elif self.direction == 3:
 			relative[0] = absolute[7]
 			relative[1] = absolute[6]
@@ -185,6 +214,9 @@ class Snake():
 			relative[4] = absolute[3]
 			relative[5] = absolute[2]
 			relative[6] = absolute[1]
+			relative[7] = absolute[11]
+			relative[8] = absolute[10]
+			relative[9] = absolute[9]
 		elif self.direction == 4:
 			relative[0] = absolute[1]
 			relative[1] = absolute[0]
@@ -193,6 +225,9 @@ class Snake():
 			relative[4] = absolute[5]
 			relative[5] = absolute[4]
 			relative[6] = absolute[3]
+			relative[7] = absolute[8]
+			relative[8] = absolute[11]
+			relative[9] = absolute[10]
 		return relative
 
 class Food:
@@ -264,15 +299,16 @@ def main():
 	failed = False
 	snakes = {}
 	for num in range(0, 100):
-		snakes[Neural_Network(6,3,1,4,[[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],[[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0]]], .2)] = 0
+		snakes[Neural_Network(14,3,1,7,[[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],[[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0]]], .2)] = 0
 	for snake in snakes:
 			snake.randomizeWeights()
-	for n in range(0, 100):
+	for n in range(0, 1000):
 		for snake in snakes:
-			snakes[snake] = run_game(10.0, snake, n)
+			snakes[snake] = run_game(10.0, snake, False)
 		sortedSnakes = sorted(snakes.items(), key=operator.itemgetter(1))
-		run_game(1000.0, sortedSnakes[-1][0], n)
+		print(n+1)
 		print(sortedSnakes[-1][1])
+		run_game(1000.0, sortedSnakes[-1][0], True)
 		snakes = {}
 		for num in range(0, 90):
 			sortedSnakes.pop(0)
@@ -286,8 +322,6 @@ def main():
 		for num in range(0, 10):
 			snakes[sortedSnakes[0][0]] = 0
 			sortedSnakes.pop(0)
-
-		print(n+1)
 
 if __name__ == "__main__":	
 	main()
